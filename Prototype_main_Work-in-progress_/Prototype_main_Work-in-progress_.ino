32ba1be643ff0bb67666fcaa37e223c533fa72ef
CJBuchel <connorbuchel@gmail.com>
2018-05-15 22:03:38 +1000

    Fixed prototype (hopefully)

--- Prototype_main_Work-in-progress_/Prototype_main_Work-in-progress_.ino
+++ Prototype_main_Work-in-progress_/Prototype_main_Work-in-progress_.ino
@@ -2,13 +2,13 @@
  
 // configure library with pins as remapped for single-channel operation
 // this lets the single motor be controlled as if it were "motor 1"
-DualVNH5019MotorShield md(2, 7, 6, A0, 2, 7, 12, A1);
+DualVNH5019MotorShield md;
  
 void setup()
 {
   md.init();
   // remaining setup code goes here
-  ...
+  
 }
  
 void loop()
