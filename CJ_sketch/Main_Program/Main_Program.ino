4a205b3f2c565bc5ec946ea21d93743315ff4b6c
CJBuchel <connorbuchel@gmail.com>
2018-05-15 20:33:37 +0800

    Started testing Motors, Created 'Main' program

---
+++ CJ_sketch/Main_Program/Main_Program.ino
@@ -0,0 +1,23 @@
+/* Arduino main program for running line follower robot.
+ *  
+ *  
+ */
+#define LED_PIN 13
+
+A4990MotorShield motors;
+
+/* 
+ * For safety, it is good practice to monitor motor driver faults and handle
+ * them in an appropriate way. If a fault is detected, both motor speeds are set
+ * to zero and it is reported on the serial port.
+ */
+void stopIfFault()
+{
+  if (motors.getFault())
+  {
+    motors.setSpeeds(0,0);
+    Serial.println("Fault");
+    while(1);
+  }
+}
+ 
