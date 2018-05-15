9bd734b33443ab11edb47972e91d5b585b1f793c
CJBuchel <connorbuchel@gmail.com>
2018-05-15 22:25:39 +1000

    Created prototype program, verifies correctly. testing required

---
+++ Motor_control_simplified_/Motor_control_simplified_.ino
@@ -0,0 +1,26 @@
+#include "DualVNH5019MotorShield.h"
+ 
+// configure library with pins as remapped for single-channel operation
+// this lets the single motor be controlled as if it were "motor 1"
+DualVNH5019MotorShield md;
+ 
+void setup()
+{
+  md.init();
+  // remaining setup code goes here
+  
+}
+ 
+void loop()
+{
+  // loops endlessly; main loop goes here
+  // the following code is a simple example:
+  md.setM1Speed(400);  // single-channel motor full-speed "forward"
+  delay(2000);  // wait for 2 seconds
+  md.setM1Speed(0);  // single-channel motor stop (coast)
+  delay(500);  // wait for 0.5 s
+  md.setM1Speed(-400);  // single-channel motor full-speed "reverse"
+  delay(2000);  // wait for 2 seconds
+  md.setM1Speed(0);  // single-channel motor stop (coast)
+  delay(500);  // wait for 0.5 s
+}
--- Prototype_main_Work-in-progress_/Prototype_main_Work-in-progress_.ino
+++ Prototype_main_Work-in-progress_/Prototype_main_Work-in-progress_.ino
@@ -1,3 +1,16 @@
+/*The tripple sensor board you have should work with this. However, the middle sesor would be obsolete.  
+
+/*------ Arduino Line Follower Code----- */
+/*-------definning Inputs------*/
+#define LS 1      // left sensor
+#define RS 13      // right sensor
+
+/*-------definning Outputs------*/
+#define LM1 2       // left motor // you can change the pin numbers to suit you best
+
+#define RM1 7       // right motor
+
+
 #include "DualVNH5019MotorShield.h"
  
 // configure library with pins as remapped for single-channel operation
@@ -8,6 +21,12 @@ void setup()
 {
   md.init();
   // remaining setup code goes here
+  pinMode(LS, INPUT);
+  pinMode(RS, INPUT);//<==sensors
+  
+  pinMode(LM1, OUTPUT);
+  
+  pinMode(RM1, OUTPUT);
   
 }
  
@@ -15,12 +34,56 @@ void loop()
 {
   // loops endlessly; main loop goes here
   // the following code is a simple example:
-  md.setM1Speed(400);  // single-channel motor full-speed "forward"
-  delay(2000);  // wait for 2 seconds
-  md.setM1Speed(0);  // single-channel motor stop (coast)
-  delay(500);  // wait for 0.5 s
-  md.setM1Speed(-400);  // single-channel motor full-speed "reverse"
-  delay(2000);  // wait for 2 seconds
-  md.setM1Speed(0);  // single-channel motor stop (coast)
-  delay(500);  // wait for 0.5 s
+
+
+ if(digitalRead(LS) && digitalRead(RS))     // Move Forward
+  {
+    
+     md.setM1Speed(400);  // single-channel motor (left) full-speed "forward"
+  delay(0);  
+
+     md.setM2Speed(400);  // single-channel motor (right) full-speed "forward"
+  delay(0);  
+    
+    
+  }
+  
+  if(!(digitalRead(LS)) && digitalRead(RS))     // Turn right
+  {
+    
+     md.setM1Speed(0);  
+  delay(0);  
+    
+    
+     md.setM2Speed(400);  
+  delay(0);  
+    
+  }
+  
+  if(digitalRead(LS) && !(digitalRead(RS)))     // turn left
+  {
+    
+     md.setM1Speed(400); 
+  delay(0);  
+   
+    
+     md.setM2Speed(0); 
+  delay(0);  
+    
+  }
+  
+  if(!(digitalRead(LS)) && !(digitalRead(RS)))     // stop
+  {
+    
+     md.setM1Speed(0);  
+  delay(0);  
+    
+    
+     md.setM1Speed(0);  
+  delay(0);  
+    
+  }
 }
+
+
+
